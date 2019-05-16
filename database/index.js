dba780d468a9feb4541d5f78735e004c6f415281
George Chen <georgechen@Georges-MacBook-Air-3.local>
2019-05-16 16:05:10 -0700

    open db connection, seed, and close connection successfully

--- database/index.js
+++ database/index.js
@@ -1,43 +1,51 @@
+/* eslint-disable no-console */
+/* eslint-disable no-plusplus */
 const mongoose = require('mongoose');
 const faker = require('faker');
 
 mongoose.connect('mongodb://localhost/house');
 
-let houseSchema = new mongoose.Schema({
-  house_id: Number,
-  pictures: [{ description: String, url: String, isVerified: Boolean }]
-});
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', () => {
+  console.log('Db connected');
 
-let House = mongoose.model('House', houseSchema); 
+  const houseSchema = new mongoose.Schema({
+    house_id: Number,
+    pictures: [{ description: String, url: String, isVerified: Boolean }],
+  });
 
-for (var i = 101; i < 201; i++) {
+  const House = mongoose.model('House', houseSchema);
 
-  let house1 = new House({
-    house_id: i,
-    pictures: []
-  })
-  
-  let randomNumBetweenRange = (min, max) => {
-    return Math.floor(Math.random() * (max - min) + min);
+  const promise = [];
+
+  for (let i = 101; i < 201; i++) {
+    const house = new House({
+      house_id: i,
+      pictures: [],
+    });
+
+    const randomNumBetweenRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+    const randomNumOfPics = randomNumBetweenRange(10, 50);
+
+    for (let j = 0; j < randomNumOfPics; j++) {
+      const randomUrlPath = Math.floor(Math.random() * 51);
+
+      const picture = {};
+      picture.description = faker.random.words();
+      picture.url = `https://s3-us-west-1.amazonaws.com/bnbpictures/pic${randomUrlPath}.jpeg`;
+      picture.isVerified = faker.random.boolean();
+
+      house.pictures.push(picture);
+    }
+    promise.push(house);
+
+    house.save();
   }
-  let randomNumOfPics = randomNumBetweenRange(8, 12);
-  
-  for (var j = 0; j < randomNumOfPics; j++) {
-  
-    let randomUrlPath = Math.floor(Math.random() * 51);
-    
-    let picture1 = {};
-    picture1['description'] = faker.random.words();
-    picture1['url'] = `https://s3-us-west-1.amazonaws.com/bnbpictures/pic${randomUrlPath}.jpeg`;
-    picture1['isVerified'] = faker.random.boolean();
-  
-    house1.pictures.push(picture1);
-  } 
-  
-  house1.save((err, house) => {
-    if (err) return console.log(err);
-    console.log('successfully saved into db')
-  })
 
-}
-
+  Promise.all(promise)
+    .then(() => {
+      mongoose.connection.close();
+      console.log('Connection closed');
+    })
+    .catch(err => console.log(err));
+});
