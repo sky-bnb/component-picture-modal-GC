28ea55c2fa3edc2234f0fea91781fb6cfd310c34
George Chen <georgechen@Georges-MacBook-Air-3.local>
2019-05-21 20:16:28 -0700

    add save button

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import PhotoGallery from './PhotoGallery.jsx';
+import Save from './Save.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,6 +23,7 @@ class App extends React.Component {
     return (
       <div>
         <PhotoGallery />
+        <Save />
       </div>
     );
   }
---
+++ client/src/components/Save.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Save extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <button className='button-save'>Save</button>
+    )
+  }
+}
+
+export default Save;
